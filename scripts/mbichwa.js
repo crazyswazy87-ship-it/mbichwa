4f4ebff3f07c89897f10629ec325bedaa9df68cb
Swazy <crazyswazy87@gmail.com>
2026-02-08 21:34:58 -0800

    startup

---
+++ scripts/mbichwa.js
@@ -0,0 +1,10 @@
+let mongo = document.querySelector('.des')
+  .addEventListener('click', mongre())
+
+function mongre() {
+setTimeout(() => {
+  let chwaki = document.querySelector('.heda') 
+    .className = 'mbichwa-active'
+  let ngo = document.querySelector('heda')
+    .className = 'heda-active'
+}, 2800);}
